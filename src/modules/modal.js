e8c40067fc86bd1665ad188da8bea9673182492d
Alexandr <alex10_08@mail.ru>
2021-12-26 11:18:16 +0100

    поправил анимацию 20 урок

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,24 +2,26 @@ const modal = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn')
     const closeBtn = modal.querySelector('.popup-close')
-
-    modal.style.opacity = '0'
+    let timer;
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
-            const width = document.documentElement.scrollWidth
             modal.style.display = 'block'
-            let timer = setInterval(() => {
-                if (modal.style.opacity === '1') clearInterval(timer);
+            modal.style.opacity = '0'
+            const animate = () => {
+                const width = document.documentElement.scrollWidth
+                timer = requestAnimationFrame(animate)
+                if (modal.style.opacity === '1') cancelAnimationFrame(timer);
                 else if (width < 768) {
                     modal.style.opacity = '1'
                 }
                 else {
                     let count = +modal.style.opacity
-                    count += 0.1
+                    count += 0.04
                     modal.style.opacity = count
                 }
-            }, 80)
+            }
+            animate()
         })
         closeBtn.addEventListener('click', () => {
             modal.style.display = 'none'
