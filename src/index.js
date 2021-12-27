6189ac6a0f53aa918d545a3ef52eb57b9a5b1154
Alexandr <alex10_08@mail.ru>
2021-12-27 23:41:58 +0100

    выполнено 21ДЗ

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import validate from './modules/validate';
 
 
 
 
-timer('31 december 2021');
+timer('31 december 2021, 24:00');
 menu()
 modal()
+validate()
---
+++ src/modules/validate.js
@@ -0,0 +1,50 @@
+const validate = () => {
+    const validCalc = document.querySelectorAll('.calc-item')
+    const validText = document.querySelectorAll('input[type="text"], input[placeholder="Ваше сообщение"]')
+    const validEmail = document.querySelectorAll('input[type="email"]')
+    const validTel = document.querySelectorAll('input[type="tel"]')
+
+    const validateCalc = (e) => {
+        let check = /\D+/g;
+        e.target.value = e.target.value.replace(check, '');
+    }
+    const validateText = (e) => {
+        let check = /[^а-яА-Я -]/g;
+        e.target.value = e.target.value.replace(check, '');
+    }
+    const validateEmail = (e) => {
+        let check = /[^\w\@\-!.'*`]+/g;
+        e.target.value = e.target.value.replace(check, '');
+    }
+    const validateTel = (e) => {
+        let check = /[^\d\-\(\)]+/g;
+        e.target.value = e.target.value.replace(check, '');
+    }
+
+    validCalc.forEach((event) => {
+        if (event[0]) {
+            return
+        }
+        else {
+            event.addEventListener('input', validateCalc)
+        }
+    })
+
+    validText.forEach((event) => {
+        if (/calc-item/g.test(event.classList.value)) {
+            return
+        }
+        else {
+            event.addEventListener('input', validateText)
+        }
+    })
+
+    validEmail.forEach((event) => {
+        event.addEventListener('input', validateEmail)
+    })
+
+    validTel.forEach((event) => {
+        event.addEventListener('input', validateTel)
+    })
+}
+export default validate
